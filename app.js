687cfebc8d6eb3bfef8e32d607ac4e8ab0813531
Tusi <1148121254@qq.com>
2018-03-20 21:00:18 +0800

    【提交人】Robin
    【提交信息】1.采用gzip压缩；

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compression = require('compression');
 
 var serverConfig = require('./config/server').CONFIG;
 var index = require('./routes/index');
 
 var app = express();
+app.use(compression());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
